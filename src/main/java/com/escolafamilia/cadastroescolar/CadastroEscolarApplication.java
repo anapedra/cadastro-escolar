c25856d9f04e2d9702e8b71afcbb230cb14806df
AnaSantana <anapedra.mil@gmail.com>
2022-10-16 17:44:10 -0300

    Initial commit

---
+++ src/main/java/com/escolafamilia/cadastroescolar/CadastroEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.escolafamilia.cadastroescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroEscolarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroEscolarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/escolafamilia/cadastroescolar/CadastroEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escolafamilia.cadastroescolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroEscolarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
